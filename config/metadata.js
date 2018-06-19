88640c7b35b0daaf1e3c485936e58d10e130c903
Abhimanyu Gupta <abhimanyu.gupta@searshc.com>
2018-06-19 16:37:54 +0530

    change in metadat file

--- config/metadata.js
+++ config/metadata.js
@@ -1,5 +1,5 @@
 const METADATA = {
-    title: 'Appliance Parts | Replacement Water Filters',
+    title: 'Appliance Parts | Replacement Water Filters | Lawn & Graden Repair Parts',
     description: 'Sears demo app project',
     host: 'localhost',
     port: 3000
