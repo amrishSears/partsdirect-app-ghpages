f5dde8b2d08f519bbe23463b29ce35279721d76c
amrishSears <40246922+amrishSears@users.noreply.github.com>
2018-06-21 11:57:05 -0500

    testing merge conflict with AG

--- config/metadata.js
+++ config/metadata.js
@@ -1,6 +1,6 @@
 const METADATA = {
     title: 'Appliance Parts | Replacement Water Filters | Lawn & Graden Repair Parts',
-    description: 'Sears demo app project',
+    description: 'Sears demo test merge conflict app project',
     host: 'localhost',
     port: 3000
 };
